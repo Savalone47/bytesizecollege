2be28972588db6dd93556305b2a5593aea9a5337
Dark Angel <jonathanyombo@gmail.com>
2021-05-05 21:34:09 +0200

    mysqli object

---
+++ college/util/connect_db.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * FILE connect_db.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 5/5/2021 - 1:21 PM
+ */
+
+$mysqli = new mysqli("localhost", "root", "", "bytesxayep_db1");
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
+function rand_string($length): string
+{
+    $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    return substr(str_shuffle($chars), 0, $length);
+}
