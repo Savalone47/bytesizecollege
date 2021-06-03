f2b7aea1c5c38e3b137e5e1de2e7f6a4015e261b
Dark Angel <jonathanyombo@gmail.com>
2021-06-03 13:17:43 +0200

    filtered_by_intake.php

---
+++ college/staff/back/filtered_by_intake.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * FILE filtered_by_intake.php
+ *
+ * @author Dark Angel <jonathanyombo@gmail.com>
+ * @copyright DATE 6/2/2021 - 11:08 PM
+ */
+
+/*
+ * DataTables example server-side processing script.
+ *
+ * Please note that this script is intentionally extremely simple to show how
+ * server-side processing can be implemented, and probably shouldn't be used as
+ * the basis for a large complex system. It is suitable for simple use cases as
+ * for learning.
+ *
+ * See http://datatables.net/usage/server-side for full details on the server-
+ * side processing requirements of DataTables.
+ *
+ * @license MIT - http://datatables.net/license_mit
+ */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Easy set variables
+ */
+
+// DB table to use
+$table = 'students';
+
+// Table's primary key
+$primaryKey = 'studentId';
+
+// Array of database columns which should be read and sent back to DataTables.
+// The `db` parameter represents the column name in the database, while the `dt`
+// parameter represents the DataTables column identifier. In this case simple
+// indexes
+$columns = array(
+    array(
+        'db' => 'studentTimestamp',
+        'dt' => 6,
+        'formatter' => function( $d, $row ) {
+            return date( 'jS M y', strtotime($d));
+        }
+    ),
+    array('db' => 'studentName', 'dt' => 1),
+    array('db' => 'studentLastName', 'dt' => 2),
+    array('db' => 'studentEmail', 'dt' => 3),
+    array('db' => 'studentNumber', 'dt' => 4),
+    array('db' => 'gender', 'dt' => 5),
+);
+
+// SQL server connection information
+$sql_details = array(
+    'user' => 'root',
+    'pass' => '',
+    'db' => 'bytesxayep_db1',
+    'host' => 'localhost'
+);
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * If you just want to use the basic configuration for DataTables with PHP
+ * server-side, there is no need to edit below this line.
+ */
+
+require('../../SSP.php');
+
+echo json_encode(
+    SSP::complex()
+);
