fc5f4ae3a9e8b52ce4840a4b9da5d1c1a1102370
Dark Angel <jonathanyombo@gmail.com>
2021-06-04 14:31:51 +0200

    old student number

--- college/staff/students_numbers.php
+++ college/staff/students_numbers.php
@@ -7,22 +7,60 @@
  */
 include "../util/connect_db.php";
 
-$sql = "SELECT * FROM students";
+set_time_limit(300);
+
+$sql = "SELECT s.studentID, c.courseCode, c.courseIntake, c.courseDepartment FROM students s, courses c, assignedcourses a WHERE a.studentID = s.studentID AND a.courseID = c.coursesID";
 $result = $mysqli->query($sql);
+
+$courses = [
+    '1000' => 'COSB',
+    '1001' => 'COSA',
+    '1002' => 'OHSF',
+    '1003' => 'ECED',
+    '1004' => 'CIPS',
+    '1005' => 'SECM',
+    '1006' => 'CISM',
+    '1008' => 'SOCW',
+    '1009' => 'PUBH'
+];
+
+$departments = [
+    '23' => 'GB',
+    '24' => 'PY',
+    '25' => 'LT',
+    '32' => 'ON',
+    '33' => 'GB',
+    '34' => 'PY',
+    '35' => 'LT'
+];
+
+$intakes = [
+    'Jan' => '01',
+    'Mar' => '03',
+    'Jun' => '06',
+    'Sep' => '09'
+];
 
 echo "<strong>started</strong>";
 
+$nb = 1;
 while ($student = $result->fetch_assoc()) {
-    $student['studentNumber'] = str_replace(' ', '', $student['studentNumber']);
+    $course = $courses["{$student['courseCode']}"];
+    $branch = $departments["{$student['courseDepartment']}"];
+    $year = "021";
 
-    $strlen = mb_strlen($student['studentNumber']);
-    $nb = ($strlen <= 15) ?  15 - $strlen : 0;
-    $studentNumber = $student['studentNumber'] . (($nb > 0) ? rand_string($nb) : "") ;
+    if (in_array($student['courseDepartment'], array('33', '34', '35'))) {
+        $year = '020';
+    }
 
-    $id = $student['studentID'];
-    $sql = "UPDATE students SET studentNumber = '{$studentNumber}' WHERE studentID = {$id}";
+    $intake = $intakes["{$student['courseIntake']}"];
+    $studentNumber = sprintf("%s%s%s%s%03d", $course, $branch, $year, $intake, $nb);
+//    var_dump($studentNumber);die;
+
+    $sql = "UPDATE students SET studentNumber = '{$studentNumber}' WHERE studentID = {$student['studentID']}";
 
     var_dump($mysqli->query($sql));
+    $nb++;
 }
 
 echo "<h1>Finish</h1>";
