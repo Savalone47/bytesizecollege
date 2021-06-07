a1e62320f3089bf1f0d868d1914b88c41f3571ad
Dark Angel <jonathanyombo@gmail.com>
2021-06-07 15:30:56 +0200

    events

--- college/staff/events.php
+++ college/staff/events.php
@@ -9,15 +9,10 @@ $json = array();
 $request = "SELECT title, CONCAT(date,' ',start) as start, CONCAT(date,' ',end) as end FROM exam ";
 
 // connection to the database
-try {
-    include "calendar/database.php";
-} catch (Exception $e) {
-    exit('Unable to connect to database.');
-}
+include "calendar/database.php";
+
 // Execute the query
 $result = $bdd->query($request) or die(print_r($bdd->errorInfo()));
 
 // sending the encoded result to success page
 echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
-
-?>
