33bcda6763ac88600126320fce929d619a312408
Dark Angel <jonathanyombo@gmail.com>
2021-06-07 15:17:36 +0200

    surround db connection with try and catch

--- college/staff/calendar/database.php
+++ college/staff/calendar/database.php
@@ -1,2 +1,6 @@
-<?php 
+<?php
+try {
 $bdd = new PDO('mysql:host=sql41.jnb1.host-h.net;dbname=bytesxayep_db1', 'bytesxayep_1', 'fqrevzbLWhKQhkAZREX8');
+} catch (Exception $e) {
+    exit('Unable to connect to database.');
+}
